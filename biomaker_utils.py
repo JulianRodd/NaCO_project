7b1c5fa7cbc84952d73caa6ff1f91b1007c2a861
JannekeNouwen <janneke@hoefnix.nl>
2024-04-20 14:02:49 +0200

    Improved structure, added good version of notebook

--- biomaker_utils.py
+++ biomaker_utils.py
@@ -1,4 +1,3 @@
-import copy
 import time
 from functools import partial
 
@@ -20,51 +19,7 @@ from self_organising_systems.biomakerca.mutators import (
     BasicMutator, RandomlyAdaptiveMutator)
 from self_organising_systems.biomakerca.step_maker import step_env
 
-
-class BaselineConfig:
-    out_file = "video_test1.mp4"
-    ec_id = "persistence"       # @param ['persistence', 'pestilence', 'collaboration', 'sideways']
-    env_width_type = "square"   # @param ['wide', 'landscape', 'square', 'petri']
-    
-    # Set soil_unbalance_limit to 0 to reproduce the original environment. Set it to 1/3 for having self-balancing environments (recommended).
-    soil_unbalance_limit = 0    # @param [0, "1/3"] {type:"raw"}
-    
-    agent_model = "minimal"     # @param ['minimal', 'extended']
-    mutator_type = "basic"      # @param ['basic', 'randomly_adaptive']
-    key = jr.PRNGKey(43)
-    
-    # How many unique programs (organisms) are allowed in the simulation.
-    n_max_programs = 3
-
-    # if True, every 50 steps we check whether the agents go extinct. If they did,
-    # we replace a seed in the environment.
-    replace_if_extinct = False
-
-    # The number of frames of the video. This is NOT the number of steps.
-    # The total number of steps depend on the number of steps per frame, which can
-    # vary over time.
-    # In the article, we generally use 500 or 750 frames.
-    n_frames = 125
-
-    # on what FRAME to double speed.
-    when_to_double_speed = [100, 200, 300, 400, 500]
-    # on what FRAME to reset speed.
-    when_to_reset_speed = []
-    fps = 20
-    # zoom_sz affects the size of the image. If this number is not even, the resulting
-    # video *may* not be supported by all renderers.
-    zoom_sz = 4
-
-    # how many steps per frame we start with. This gets usually doubled many times
-    # during the simulation.
-    # In the article, we usually use 2 or 4 as the starting value, sometimes 1.
-    steps_per_frame = 2
-    
-    ### Evaluation ###
-    what_to_evaluate = "extracted"  # @param ["initialization", "extracted"]
-    n_eval_steps = 100
-    n_eval_reps = 1
-    eval_key = jr.PRNGKey(123)
+from configs.base_config import BaselineConfig
 
 def pad_text(img, text):
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -200,7 +155,7 @@ def evaluate_biome(
     return tot_agents_n, is_extinct
 
 
-def evaluation(env, programs, st_env, env_config, agent_logic, mutator, base_config):
+def perform_evaluation(env, programs, st_env, env_config, agent_logic, mutator, base_config):
     # Evaluate the configuration
     # With this code, you can either evaluate a randomly initialized model, or models extracted from the previous run.
     # In the latter case, make sure that there are agents alive at the end of the simulation.
@@ -272,7 +227,7 @@ def main():
 
     perform_simulation(env, programs, base_config, env_config, agent_logic, mutator, key)
 
-    evaluation(env, programs, st_env, env_config, agent_logic, mutator, base_config)
+    perform_evaluation(env, programs, st_env, env_config, agent_logic, mutator, base_config)
     
 
 if __name__ == "__main__":
@@ -284,6 +239,7 @@ if __name__ == "__main__":
 ## Regardless, to trigger the recomputation of step_env and similar,
 ## config needs to be a new object! So, first, we create a new copy.
 
+# import copy
 # env_config = copy.copy(env_config)
 
 ## Change simple isolated parameters (most of them)
